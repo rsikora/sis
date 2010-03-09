07b1922c6953a034c2af5d02cd00ecf6049d0a43
unknown <der3k@.(none)>
2010-03-09 16:03:54 +0100

    implementing Amount

--- modules/sis-domain/src/main/java/cz/snet/domain/model/account/Charge.java
+++ modules/sis-domain/src/main/java/cz/snet/domain/model/account/Charge.java
@@ -3,5 +3,4 @@ package cz.snet.domain.model.account;
 import cz.snet.domain.shared.ValueObject;
 
 public class Charge implements ValueObject<Charge> {
-
 }
--- modules/sis-domain/src/main/java/cz/snet/domain/model/invoice/Amount.java
+++ modules/sis-domain/src/main/java/cz/snet/domain/model/invoice/Amount.java
@@ -2,6 +2,50 @@ package cz.snet.domain.model.invoice;
 
 import cz.snet.domain.shared.ValueObject;
 
+import java.math.BigDecimal;
+
 public class Amount implements ValueObject<Amount> {
+
+    private final static int ROUNDING = BigDecimal.ROUND_HALF_UP;
+
+    private final BigDecimal net;
+    private final BigDecimal taxPct;
+    private final BigDecimal tax;
+    private final BigDecimal brt;
+
+    private Amount(BigDecimal net, BigDecimal taxPct) {
+        this.net = normalizeScale(net);
+        this.taxPct = taxPct;
+        this.tax = normalizeScale(net.multiply(taxPct).movePointLeft(2));
+        brt = net.add(tax);
+    }
+
+    public static Amount from(BigDecimal net) {
+        return new Amount(net, BigDecimal.ZERO);
+    }
+
+    public static Amount from(BigDecimal net, BigDecimal taxPct) {
+        return new Amount(net, taxPct);
+    }
+
+    private BigDecimal normalizeScale(BigDecimal value) {
+        return value.setScale(2, ROUNDING);
+    }
+
+    public BigDecimal net() {
+        return net;
+    }
+
+    public BigDecimal tax() {
+        return tax;
+    }
+
+    public BigDecimal taxPct() {
+        return taxPct;
+    }
+
+    public BigDecimal brt() {
+        return brt;
+    }
 
 }
