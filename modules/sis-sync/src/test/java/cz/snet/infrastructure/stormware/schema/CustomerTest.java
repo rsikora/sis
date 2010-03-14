4a7294eba47029ffb3ae1c21ffb95dda42044108
unknown <der3k@.(none)>
2010-03-14 22:57:10 +0100

    added StAX looping for unmarshalling

--- modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/CustomerTest.java
+++ modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/CustomerTest.java
@@ -1,12 +1,16 @@
 package cz.snet.infrastructure.stormware.schema;
 
 import cz.stormware.schema.addressbook.AddressbookResponseType;
+import cz.stormware.schema.response.ResponsePackItemType;
 import cz.stormware.schema.response.ResponsePackType;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
 import java.io.InputStream;
 
@@ -21,12 +25,26 @@ import static org.junit.Assert.assertThat;
 public class CustomerTest {
     @Test
     public void testUnmarshalMapping() throws Exception {
+        // create unmarshaller
         JAXBContext context = JAXBContext.newInstance("cz.stormware.schema.response:cz.stormware.schema.list:cz.stormware.schema.addressbook:cz.stormware.schema.documentresponse");
         Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        // configure StAX parser
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
         InputStream customers = ClassLoader.getSystemResourceAsStream("xml/customers-response-20100313.xml");
-        JAXBElement responsePackElement = (JAXBElement) unmarshaller.unmarshal(customers);
-        ResponsePackType responsePack = (ResponsePackType) responsePackElement.getValue();
-        System.out.println(responsePack.getResponsePackItem().get(0).getAddressbookResponse().getProducedDetails().getId());
-        System.out.println(responsePack.getResponsePackItem().get(0).getAddressbookResponse().getProducedDetails().getCode());
+        XMLStreamReader streamReader = inputFactory.createXMLStreamReader(customers);
+
+        // move to the first item
+        streamReader.nextTag();
+        streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePack");
+        streamReader.nextTag();
+
+        // loop over the items
+        while(streamReader.getEventType() == XMLStreamConstants.START_ELEMENT) {
+            streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePackItem");
+            JAXBElement<ResponsePackItemType> customerResponse = unmarshaller.unmarshal(streamReader, ResponsePackItemType.class);
+            System.out.println(customerResponse.getValue().getAddressbookResponse().getProducedDetails().getCode());
+            streamReader.nextTag();
+        }
     }
 }
