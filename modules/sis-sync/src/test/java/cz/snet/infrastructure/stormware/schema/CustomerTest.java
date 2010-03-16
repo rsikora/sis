0621b882fb8d8adec06483c75bf4291bb25a0f36
der3k <rs.pipe@gmail.com>
2010-03-16 17:54:08 +0100

    tunned the tests

--- modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/CustomerTest.java
+++ modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/CustomerTest.java
@@ -1,15 +1,11 @@
 package cz.snet.infrastructure.stormware.schema;
 
 import cz.stormware.schema.addressbook.AddressbookHeaderType;
-import cz.stormware.schema.addressbook.AddressbookResponseType;
 import cz.stormware.schema.addressbook.AddressbookType;
 import cz.stormware.schema.data.DataPackItemType;
 import cz.stormware.schema.data.DataPackType;
-import cz.stormware.schema.invoice.InvoiceHeaderType;
-import cz.stormware.schema.invoice.InvoiceType;
-import cz.stormware.schema.invoice.InvoiceTypeType;
 import cz.stormware.schema.response.ResponsePackItemType;
-import cz.stormware.schema.response.ResponsePackType;
+import org.apache.log4j.Logger;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
@@ -22,9 +18,9 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
-import java.io.File;
-import java.io.InputStream;
+import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.LinkedList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -35,6 +31,8 @@ import static org.junit.Assert.assertThat;
  * Time: 10:59:40 PM
  */
 public class CustomerTest {
+
+    private static final Logger log = Logger.getLogger(InvoiceTest.class);
 
     @Test
     public void testMarshalMapping() throws Exception {
@@ -54,7 +52,7 @@ public class CustomerTest {
 
         DataPackType dataPack = new DataPackType();
         dataPack.getDataPackItem().add(dataPackItem);
-        JAXBElement dataPackElement = new JAXBElement(new QName("dataPack"), DataPackType.class, dataPack);
+        JAXBElement<DataPackType> dataPackElement = new JAXBElement<DataPackType>(new QName("dataPack"), DataPackType.class, dataPack);
 
         StringWriter writer = new StringWriter();
         marshaller.marshal(dataPackElement, writer);
@@ -70,8 +68,8 @@ public class CustomerTest {
                 "  </dat:dataPackItem>\n" +
                 "</dataPack>";
 
-        System.out.println(expected);
-        System.out.println(writer);
+        log.debug("EXPECTED:\n" + expected);
+        log.debug("ACTUAL:\n" + writer.toString());
 
         XMLUnit.setIgnoreAttributeOrder(true);
         XMLUnit.setIgnoreWhitespace(true);
@@ -82,26 +80,54 @@ public class CustomerTest {
 
     @Test
     public void testUnmarshalMapping() throws Exception {
+
+        String customersResponseXml = "" +
+                "<?xml version=\"1.0\" encoding=\"Windows-1250\"?>\n" +
+                "<rsp:responsePack version=\"1.0\" id=\"2010-03-08T10:37:25.152\" state=\"ok\" note=\"\" programVersion=\"9403.5 E1 (17.2.2010)\" xmlns:rsp=\"http://www.stormware.cz/schema/response.xsd\" xmlns:rdc=\"http://www.stormware.cz/schema/documentresponse.xsd\" xmlns:typ=\"http://www.stormware.cz/schema/type.xsd\" xmlns:lst=\"http://www.stormware.cz/schema/list.xsd\" xmlns:acu=\"http://www.stormware.cz/schema/accountingunit.xsd\" xmlns:inv=\"http://www.stormware.cz/schema/invoice.xsd\" xmlns:vch=\"http://www.stormware.cz/schema/voucher.xsd\" xmlns:int=\"http://www.stormware.cz/schema/intDoc.xsd\" xmlns:stk=\"http://www.stormware.cz/schema/stock.xsd\" xmlns:ord=\"http://www.stormware.cz/schema/order.xsd\" xmlns:ofr=\"http://www.stormware.cz/schema/offer.xsd\" xmlns:enq=\"http://www.stormware.cz/schema/enquiry.xsd\" xmlns:vyd=\"http://www.stormware.cz/schema/vydejka.xsd\" xmlns:pri=\"http://www.stormware.cz/schema/prijemka.xsd\" xmlns:bal=\"http://www.stormware.cz/schema/balance.xsd\" xmlns:pre=\"http://www.stormware.cz/schema/prevodka.xsd\" xmlns:vyr=\"http://www.stormware.cz/schema/vyroba.xsd\" xmlns:pro=\"http://www.stormware.cz/schema/prodejka.xsd\" xmlns:con=\"http://www.stormware.cz/schema/contract.xsd\" xmlns:adb=\"http://www.stormware.cz/schema/addressbook.xsd\" xmlns:prm=\"http://www.stormware.cz/schema/parameter.xsd\" xmlns:lst2=\"http://www.stormware.cz/schema/list_2.xsd\" xmlns:ctg=\"http://www.stormware.cz/schema/category.xsd\" xmlns:ipm=\"http://www.stormware.cz/schema/intParam.xsd\">\n" +
+                "<rsp:responsePackItem version=\"1.0\" id=\"2010-03-08T10:37:25.152_0000000000_6237\" state=\"ok\">\n" +
+                "\t<adb:addressbookResponse version=\"1.5\" state=\"ok\">\n" +
+                "\t\t<rdc:producedDetails>\n" +
+                "\t\t\t<rdc:id>9226</rdc:id>\n" +
+                "\t\t\t<rdc:code>Nieslanik Jan</rdc:code>\n" +
+                "\t\t</rdc:producedDetails>\n" +
+                "\t</adb:addressbookResponse>\n" +
+                "</rsp:responsePackItem>\n" +
+                "<rsp:responsePackItem version=\"1.0\" id=\"2010-03-08T10:37:25.152_0000000001_5680\" state=\"ok\">\n" +
+                "\t<adb:addressbookResponse version=\"1.5\" state=\"ok\">\n" +
+                "\t\t<rdc:producedDetails>\n" +
+                "\t\t\t<rdc:id>8707</rdc:id>\n" +
+                "\t\t\t<rdc:code>Wawreczka David</rdc:code>\n" +
+                "\t\t</rdc:producedDetails>\n" +
+                "\t</adb:addressbookResponse>\n" +
+                "</rsp:responsePackItem>\n" +
+                "</rsp:responsePack>\n" +
+                "";
+
         // create unmarshaller
         JAXBContext context = JAXBContext.newInstance("cz.stormware.schema.response");
         Unmarshaller unmarshaller = context.createUnmarshaller();
 
         // configure StAX parser
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-        InputStream customers = ClassLoader.getSystemResourceAsStream("xml/customers-response-20100313.xml");
-        XMLStreamReader streamReader = inputFactory.createXMLStreamReader(customers);
+        XMLStreamReader streamReader = inputFactory.createXMLStreamReader(new StringReader(customersResponseXml));
 
         // move to the first item
         streamReader.nextTag();
         streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePack");
         streamReader.nextTag();
 
-        // loop over the items
-        while(streamReader.getEventType() == XMLStreamConstants.START_ELEMENT) {
+        String[] expectedNames = new String[] {"Nieslanik Jan", "Wawreczka David"};
+        
+        // loop over customer responses
+        LinkedList<String> names = new LinkedList<String>();
+        while (streamReader.getEventType() == XMLStreamConstants.START_ELEMENT) {
             streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePackItem");
             JAXBElement<ResponsePackItemType> customerResponse = unmarshaller.unmarshal(streamReader, ResponsePackItemType.class);
-            System.out.println(customerResponse.getValue().getAddressbookResponse().getProducedDetails().getCode());
+            String name = customerResponse.getValue().getAddressbookResponse().getProducedDetails().getCode();
+            log.debug(name);
+            names.add(name);
             streamReader.nextTag();
         }
+        assertThat(names.toArray(new String[names.size()]), is(expectedNames));
     }
 }
--- modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/InvoiceTest.java
+++ modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/InvoiceTest.java
@@ -6,6 +6,7 @@ import cz.stormware.schema.invoice.InvoiceHeaderType;
 import cz.stormware.schema.invoice.InvoiceType;
 import cz.stormware.schema.invoice.InvoiceTypeType;
 import cz.stormware.schema.response.ResponsePackItemType;
+import org.apache.log4j.Logger;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
@@ -19,7 +20,9 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.LinkedList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -30,6 +33,8 @@ import static org.junit.Assert.assertThat;
  * Time: 10:59:40 PM
  */
 public class InvoiceTest {
+
+    private static final Logger log = Logger.getLogger(InvoiceTest.class);
 
     @Test
     public void testMarshalMapping() throws Exception {
@@ -66,8 +71,8 @@ public class InvoiceTest {
                 "  </dat:dataPackItem>\n" +
                 "</dataPack>";
 
-        System.out.println(expected);
-        System.out.println(writer);
+        log.debug("EXPECTED:\n" + expected);
+        log.debug("ACTUAL:\n" + writer.toString());
 
         XMLUnit.setIgnoreAttributeOrder(true);
         XMLUnit.setIgnoreWhitespace(true);
@@ -78,6 +83,69 @@ public class InvoiceTest {
 
     @Test
     public void testUnmarshalMapping() throws Exception {
+        String invoicesResponseXml = "" +
+                "<?xml version=\"1.0\" encoding=\"Windows-1250\"?>\n" +
+                "<rsp:responsePack version=\"1.0\" id=\"2010-03-10T17:02:48.593\" state=\"ok\" note=\"\" programVersion=\"9403.5 E1 (17.2.2010)\" xmlns:rsp=\"http://www.stormware.cz/schema/response.xsd\" xmlns:rdc=\"http://www.stormware.cz/schema/documentresponse.xsd\" xmlns:typ=\"http://www.stormware.cz/schema/type.xsd\" xmlns:lst=\"http://www.stormware.cz/schema/list.xsd\" xmlns:acu=\"http://www.stormware.cz/schema/accountingunit.xsd\" xmlns:inv=\"http://www.stormware.cz/schema/invoice.xsd\" xmlns:vch=\"http://www.stormware.cz/schema/voucher.xsd\" xmlns:int=\"http://www.stormware.cz/schema/intDoc.xsd\" xmlns:stk=\"http://www.stormware.cz/schema/stock.xsd\" xmlns:ord=\"http://www.stormware.cz/schema/order.xsd\" xmlns:ofr=\"http://www.stormware.cz/schema/offer.xsd\" xmlns:enq=\"http://www.stormware.cz/schema/enquiry.xsd\" xmlns:vyd=\"http://www.stormware.cz/schema/vydejka.xsd\" xmlns:pri=\"http://www.stormware.cz/schema/prijemka.xsd\" xmlns:bal=\"http://www.stormware.cz/schema/balance.xsd\" xmlns:pre=\"http://www.stormware.cz/schema/prevodka.xsd\" xmlns:vyr=\"http://www.stormware.cz/schema/vyroba.xsd\" xmlns:pro=\"http://www.stormware.cz/schema/prodejka.xsd\" xmlns:con=\"http://www.stormware.cz/schema/contract.xsd\" xmlns:adb=\"http://www.stormware.cz/schema/addressbook.xsd\" xmlns:prm=\"http://www.stormware.cz/schema/parameter.xsd\" xmlns:lst2=\"http://www.stormware.cz/schema/list_2.xsd\" xmlns:ctg=\"http://www.stormware.cz/schema/category.xsd\" xmlns:ipm=\"http://www.stormware.cz/schema/intParam.xsd\">\n" +
+                "  <rsp:responsePackItem version=\"1.0\" id=\"2010-03-10T17:02:48.593_0000000000_134409\" state=\"ok\">\n" +
+                "    <inv:invoiceResponse version=\"1.3\" state=\"ok\">\n" +
+                "      <rdc:importDetails>\n" +
+                "        <rdc:detail>\n" +
+                "          <rdc:state>warning</rdc:state>\n" +
+                "          <rdc:errno>606</rdc:errno>\n" +
+                "          <rdc:note>Datum není z úèetního období.</rdc:note>\n" +
+                "          <rdc:XPath>dat:dataPack/dat:dataPackItem[1]/inv:invoice/inv:invoiceHeader/inv:date</rdc:XPath>\n" +
+                "          <rdc:valueRequested>08.03.2010</rdc:valueRequested>\n" +
+                "          <rdc:valueProduced>08.03.2010</rdc:valueProduced>\n" +
+                "        </rdc:detail>\n" +
+                "        <rdc:detail>\n" +
+                "          <rdc:state>warning</rdc:state>\n" +
+                "          <rdc:errno>606</rdc:errno>\n" +
+                "          <rdc:note>Datum mùže být nejvýše 31.12.2009.\n" +
+                "\n" +
+                "Tip: Pokud chcete zadat doklad z poèátku dalšího období,\n" +
+                "nastavte Úèetnictví/Uzávìrka/Úètování na pøelomu úèetního období.</rdc:note>\n" +
+                "          <rdc:XPath>dat:dataPack/dat:dataPackItem[1]/inv:invoice/inv:invoiceHeader/inv:date</rdc:XPath>\n" +
+                "          <rdc:valueRequested>08.03.2010</rdc:valueRequested>\n" +
+                "          <rdc:valueProduced>08.03.2010</rdc:valueProduced>\n" +
+                "        </rdc:detail>\n" +
+                "      </rdc:importDetails>\n" +
+                "      <rdc:producedDetails>\n" +
+                "        <rdc:id>60925</rdc:id>\n" +
+                "        <rdc:number>2010008197</rdc:number>\n" +
+                "      </rdc:producedDetails>\n" +
+                "    </inv:invoiceResponse>\n" +
+                "  </rsp:responsePackItem>\n" +
+                "  <rsp:responsePackItem version=\"1.0\" id=\"2010-03-10T17:02:48.593_0000000001_134410\" state=\"ok\">\n" +
+                "    <inv:invoiceResponse version=\"1.3\" state=\"ok\">\n" +
+                "      <rdc:importDetails>\n" +
+                "        <rdc:detail>\n" +
+                "          <rdc:state>warning</rdc:state>\n" +
+                "          <rdc:errno>606</rdc:errno>\n" +
+                "          <rdc:note>Datum není z úèetního období.</rdc:note>\n" +
+                "          <rdc:XPath>dat:dataPack/dat:dataPackItem[2]/inv:invoice/inv:invoiceHeader/inv:date</rdc:XPath>\n" +
+                "          <rdc:valueRequested>08.03.2010</rdc:valueRequested>\n" +
+                "          <rdc:valueProduced>08.03.2010</rdc:valueProduced>\n" +
+                "        </rdc:detail>\n" +
+                "        <rdc:detail>\n" +
+                "          <rdc:state>warning</rdc:state>\n" +
+                "          <rdc:errno>606</rdc:errno>\n" +
+                "          <rdc:note>Datum mùže být nejvýše 31.12.2009.\n" +
+                "\n" +
+                "Tip: Pokud chcete zadat doklad z poèátku dalšího období,\n" +
+                "nastavte Úèetnictví/Uzávìrka/Úètování na pøelomu úèetního období.</rdc:note>\n" +
+                "          <rdc:XPath>dat:dataPack/dat:dataPackItem[2]/inv:invoice/inv:invoiceHeader/inv:date</rdc:XPath>\n" +
+                "          <rdc:valueRequested>08.03.2010</rdc:valueRequested>\n" +
+                "          <rdc:valueProduced>08.03.2010</rdc:valueProduced>\n" +
+                "        </rdc:detail>\n" +
+                "      </rdc:importDetails>\n" +
+                "      <rdc:producedDetails>\n" +
+                "        <rdc:id>60926</rdc:id>\n" +
+                "        <rdc:number>2010008198</rdc:number>\n" +
+                "      </rdc:producedDetails>\n" +
+                "    </inv:invoiceResponse>\n" +
+                "  </rsp:responsePackItem>\n" +
+                "</rsp:responsePack>\n" +
+                "";
 
         // create unmarshaller
         JAXBContext context = JAXBContext.newInstance("cz.stormware.schema.response");
@@ -85,21 +153,28 @@ public class InvoiceTest {
 
         // configure StAX parser
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-        InputStream customers = ClassLoader.getSystemResourceAsStream("xml/invoices-response-20100313.xml");
-        XMLStreamReader streamReader = inputFactory.createXMLStreamReader(customers);
+        XMLStreamReader streamReader = inputFactory.createXMLStreamReader(new StringReader(invoicesResponseXml));
 
         // move to the first item
         streamReader.nextTag();
         streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePack");
         streamReader.nextTag();
 
-        // loop over the items
-        while(streamReader.getEventType() == XMLStreamConstants.START_ELEMENT) {
+        String[] expectedNumbers = new String[] {"2010008197 WARNING", "2010008198 WARNING"};
+
+        // loop over the invoice responses
+        LinkedList<String> numbers = new LinkedList<String>();
+        while (streamReader.getEventType() == XMLStreamConstants.START_ELEMENT) {
             streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePackItem");
-            JAXBElement<ResponsePackItemType> customerResponse = unmarshaller.unmarshal(streamReader, ResponsePackItemType.class);
-            System.out.println(customerResponse.getValue().getInvoiceResponse().getProducedDetails().getNumber());
-            System.out.println(customerResponse.getValue().getInvoiceResponse().getImportDetails().getDetail().get(0).getState());
+            JAXBElement<ResponsePackItemType> invoiceResponse = unmarshaller.unmarshal(streamReader, ResponsePackItemType.class);
+            StringBuilder number = new StringBuilder(invoiceResponse.getValue().getInvoiceResponse().getProducedDetails().getNumber());
+            number.append(" ").
+                    append(invoiceResponse.getValue().getInvoiceResponse().getImportDetails().getDetail().get(0).getState());
+            String numberString = number.toString();
+            log.debug(numberString);
+            numbers.add(numberString);
             streamReader.nextTag();
         }
+        assertThat(numbers.toArray(new String[numbers.size()]), is(expectedNumbers));
     }
 }
