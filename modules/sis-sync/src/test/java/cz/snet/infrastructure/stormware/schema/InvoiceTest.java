f1049ce745c4367e3874a667e56d75efab1dd516
der3k <rs.pipe@gmail.com>
2010-03-16 14:36:44 +0100

    added invoice response unmarshalling mapping test

--- modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/InvoiceTest.java
+++ modules/sis-sync/src/test/java/cz/snet/infrastructure/stormware/schema/InvoiceTest.java
@@ -1,8 +1,6 @@
 package cz.snet.infrastructure.stormware.schema;
 
-import cz.stormware.schema.addressbook.AddressbookResponseType;
 import cz.stormware.schema.response.ResponsePackItemType;
-import cz.stormware.schema.response.ResponsePackType;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBContext;
@@ -11,28 +9,25 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
-import java.io.File;
 import java.io.InputStream;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 /**
  * User: der3k
  * Date: Mar 13, 2010
  * Time: 10:59:40 PM
  */
-public class CustomerTest {
+public class InvoiceTest {
 
     @Test
     public void testUnmarshalMapping() throws Exception {
+
         // create unmarshaller
-        JAXBContext context = JAXBContext.newInstance("cz.stormware.schema.response:cz.stormware.schema.list:cz.stormware.schema.addressbook:cz.stormware.schema.documentresponse");
+        JAXBContext context = JAXBContext.newInstance("cz.stormware.schema.response:cz.stormware.schema.list:cz.stormware.schema.invoice:cz.stormware.schema.documentresponse");
         Unmarshaller unmarshaller = context.createUnmarshaller();
 
         // configure StAX parser
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-        InputStream customers = ClassLoader.getSystemResourceAsStream("xml/customers-response-20100313.xml");
+        InputStream customers = ClassLoader.getSystemResourceAsStream("xml/invoices-response-20100313.xml");
         XMLStreamReader streamReader = inputFactory.createXMLStreamReader(customers);
 
         // move to the first item
@@ -44,7 +39,8 @@ public class CustomerTest {
         while(streamReader.getEventType() == XMLStreamConstants.START_ELEMENT) {
             streamReader.require(XMLStreamConstants.START_ELEMENT, null, "responsePackItem");
             JAXBElement<ResponsePackItemType> customerResponse = unmarshaller.unmarshal(streamReader, ResponsePackItemType.class);
-            System.out.println(customerResponse.getValue().getAddressbookResponse().getProducedDetails().getCode());
+            System.out.println(customerResponse.getValue().getInvoiceResponse().getProducedDetails().getNumber());
+            System.out.println(customerResponse.getValue().getInvoiceResponse().getImportDetails().getDetail().get(0).getState());
             streamReader.nextTag();
         }
     }
